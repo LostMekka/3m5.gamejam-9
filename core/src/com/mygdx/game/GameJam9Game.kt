11003d59a9aba790e1caf1726c09ac0b20dd60c2
3m5.Stefan Beyer <Stefan.Beyer@3m5.de>
2022-10-30 14:17:54 +0100

    BÖÖP

--- core/src/com/mygdx/game/GameJam9Game.kt
+++ core/src/com/mygdx/game/GameJam9Game.kt
@@ -3,7 +3,6 @@ package com.mygdx.game
 import com.badlogic.gdx.Screen
 import com.badlogic.gdx.assets.AssetManager
 import com.mygdx.game.assets.AssetDescriptors
-import com.mygdx.game.common.soundController
 import com.mygdx.game.screens.GameScreen
 import com.mygdx.game.screens.SplashScreen
 import com.mygdx.game.ui.initUi
@@ -20,9 +19,6 @@ class MyGdxGame : KtxGame<Screen>() {
         addScreen(GameScreen())
         addScreen(SplashScreen(this))
         setScreen<GameScreen>()
-
-        // just for testing, don't be scared!
-        soundController.playTestSound()
     }
 
     private fun loadAssets() {
